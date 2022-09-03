cec729efe27a1fd72e4e347a086697959d354ec3
cti23gy <70219724+cti23gy@users.noreply.github.com>
2022-09-03 13:39:23 -0400

    https fix

--- app/model.js
+++ app/model.js
@@ -1,8 +1,9 @@
-var baseURL = "http://api.weatherapi.com/v1/";
+var baseURL = "https://api.weatherapi.com/v1/";
 var key = "2af2ca9438e742d1be7202359222908";
 
 function getCurrentDate() {
     //console.log(utility(name));
+
 
     const date = new Date();
     const year = date.getFullYear();
@@ -30,7 +31,7 @@ function getCurrentWeather(location, numDays) {
 
     //for retrieving api info
     const currentDate = getCurrentDate()
-    $.get(`${baseURL}forecast.json?key=${key}&q=${location}&days=${numDays}&aqi=no&alerts=no`, //FIX LINK TO MATCH SEARCH QUERY!!
+    $.get(`${baseURL}forecast.json?key=${key}&q=${location}&days=${numDays}&aqi=no&alerts=no`, 
     (data) => {
         console.log(data);
         console.log(currentDate);
